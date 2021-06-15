6c56f82f321861f8f14b0b0a0a5ff6aacc05fbb7
Ignacio Farias Valladares <ifarias@koandina.com>
2021-06-15 15:47:19 -0400

    Changed main.yml file

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello M3n!')
+        'body': json.dumps("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
     }
